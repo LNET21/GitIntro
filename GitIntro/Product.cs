795074de79de83b214b1a0101152f342fe859a2b
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-08-18 13:44:08 +0200

    Changes again

--- GitIntro/Product.cs
+++ GitIntro/Product.cs
@@ -4,11 +4,6 @@
     {
         public string TitleName { get; set; }
         public string Shelf { get; set; }
-        public int FirstNumber { get; set; }
-        public int MyProperty { get; set; }
-        public string Name { get; set; }
-        public int Nr { get; set; }
-        public string Category { get; set; }
-        public int Count { get; set; }
+        public int Count2 { get; set; }
     }
 }
