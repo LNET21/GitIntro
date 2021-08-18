9639a40ae595fc100fd3717fa9acea6c2b737225
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-08-18 13:50:25 +0200

    Revert "Changes again"
    This reverts commit 795074de79de83b214b1a0101152f342fe859a2b.

--- GitIntro/Product.cs
+++ GitIntro/Product.cs
@@ -4,6 +4,11 @@
     {
         public string TitleName { get; set; }
         public string Shelf { get; set; }
-        public int Count2 { get; set; }
+        public int FirstNumber { get; set; }
+        public int MyProperty { get; set; }
+        public string Name { get; set; }
+        public int Nr { get; set; }
+        public string Category { get; set; }
+        public int Count { get; set; }
     }
 }
